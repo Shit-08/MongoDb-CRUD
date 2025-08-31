1bae64803089da57b06a66eb74f857a76c21a829
Shital Patil <patilshitalbharat@gmail.com>
2025-08-31 22:25:04 +0530

    added crud routes

--- index.js
+++ index.js
@@ -6,7 +6,8 @@ const Chat1= require('./models/chat')
 
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")))
+app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({extended: true}));
 
 main()
 .then(()=>{
@@ -38,8 +39,35 @@ app.get('/',(req,res)=>{
 app.get('/chats',async (req,res)=>{
     let chats= await Chat1.find();
     res.render("index.ejs", {chats});
+});
+
+//New Route
+app.get('/chats/new',(req,res)=>{
+    res.render("new.ejs");
+});
+
+//Create Route
+app.post('/chats',(req,res)=>{
+    let {from, msg, to}=req.body;
+    let newChat= new Chat1({
+        from: from,
+        to: to,
+        msg: msg,
+        created_at: new Date()
+    })
+    newChat.save()
+    .then(res=> console.log("chat was saved"))
+    .catch(err=> console.log(err));
+    res.redirect("/chats");
 })
+
+//Edit Route
+app.get('/chats/:id/edit',async(req,res)=>{
+    let id=req.params.id;
+    let chat= await Chat1.findById(id);
+    res.render("edit.ejs", {chat} );
+});
 
 app.listen(8080, ()=>{
     console.log("server is listening on port 8008");
-})
+});
